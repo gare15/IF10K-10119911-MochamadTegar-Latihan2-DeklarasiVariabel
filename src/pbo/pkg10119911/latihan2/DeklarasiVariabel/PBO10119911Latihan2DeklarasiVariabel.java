c03daff7b66998e1a31e1687b50a3cab6f1d4fd4
gare15 <72866876+gare15@users.noreply.github.com>
2020-10-21 08:19:01 +0700

    DeklarasiVariabel

---
+++ src/pbo/pkg10119911/latihan2/DeklarasiVariabel/PBO10119911Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10119911.latihan2.deklarasivariabel;
+
+/**
+ * 
+ * @author 
+ * NAMA     : Mochamad Tegar Dwi S
+ * KELAS    : IF10K
+ * NIM      : 10119911
+ * Deskripsi Program : Deklarasi Variabel
+ */
+public class PBO10119911Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 76;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println();
+        System.out.println(" Isi variabel nilai = " + nilaiInt);
+        System.out.println(" Isi variabel PHI = " + PHI);
+        System.out.println(" Isi Variabel logika = " + nilaiLogika);
+        System.out.println(" Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
